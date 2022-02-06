9a61fb0cab44390290f3ec51c9831ecd2fd9edfd
Apiglio <apiglioetapemiro@gmail.com>
2022-02-06 19:13:36 +0800

    0.1.0-alpha.2

--- rtfp_main.pas
+++ rtfp_main.pas
@@ -6,14 +6,14 @@ interface
 
 uses
   Classes, SysUtils, db, dbf, FileUtil, Forms, Controls, Graphics, Dialogs,
-  ComCtrls, Menus, ExtCtrls, DBGrids, Grids, ValEdit, LazUTF8,
+  ComCtrls, Menus, ExtCtrls, DBGrids, Grids, ValEdit, CheckLst, LazUTF8,
 
   Apiglio_Useful, AufScript_Frame,
 
   RTFP_definition;
 
 const
-  C_VERSION_NUMBER  = '0.1.0-alpha.1';
+  C_VERSION_NUMBER  = '0.1.0-alpha.2';
   C_SOFTWARE_NAME   = 'RTFP Desktop';
   C_SOFTWARE_AUTHOR = 'Apiglio';
 
@@ -23,6 +23,7 @@ type
   { TFormDesktop }
 
   TFormDesktop = class(TForm)
+    CheckListBox_MainAttrFilter: TCheckListBox;
     DBGrid_Main: TDBGrid;
     Frame_AufScript1: TFrame_AufScript;
     MainMenu: TMainMenu;
@@ -65,6 +66,7 @@ type
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCloseQuery(Sender: TObject; var CanClose: boolean);
     procedure FormCreate(Sender: TObject);
+    procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
     procedure FormResize(Sender: TObject);
     procedure MenuItem_option_aboutClick(Sender: TObject);
     procedure MenuItem_project_closeClick(Sender: TObject);
@@ -78,7 +80,18 @@ type
   private
 
   public
-    procedure Validate(Sender:TObject);//更新显示，Sender参数为TRTFP类
+    //RTFP类事件，Sender参数为TRTFP类
+    procedure EventLink(Sender:TRTFP);//链接所有事件
+
+    procedure Validate(Sender:TObject);//更新显示
+    procedure FirstEdit(Sender:TObject);//工程第一次编辑
+    procedure Clear(Sender:TObject);//清空
+
+    procedure ProjectOpenDone(Sender:TObject);//工程打开或新建
+    procedure ProjectCloseDone(Sender:TObject);//工程关闭
+    procedure ProjectSaveDone(Sender:TObject);//工程保存
+
+
 
 
   end;
@@ -94,12 +107,24 @@ uses form_new_project;
 
 { TFormDesktop }
 
+procedure TFormDesktop.EventLink(Sender:TRTFP);//链接所有事件
+begin
+  Sender.onNewDone:=@ProjectOpenDone;
+  Sender.onOpenDone:=@ProjectOpenDone;
+  Sender.onFirstEdit:=@FirstEdit;
+  Sender.onSaveDone:=@ProjectSaveDone;
+  Sender.onCloseDone:=@ProjectCloseDone;
+end;
 
 procedure TFormDesktop.Validate(Sender:TObject);
+var changed_str:string;
 begin
 
+  if (Sender as TRTFP).IsChanged then changed_str:=' *'
+  else changed_str:='';
+
   if (Sender as TRTFP).Title <> '' then
-    Self.Caption:=C_SOFTWARE_NAME+' - '+(Sender as TRTFP).Title
+    Self.Caption:=C_SOFTWARE_NAME+' - '+(Sender as TRTFP).Title + changed_str
   else
     Self.Caption:=C_SOFTWARE_NAME;
 
@@ -109,14 +134,59 @@ begin
   Self.PropertiesValueListEditor.Values['创建日期']:=(Sender as TRTFP).Tag['创建日期'];
   Self.PropertiesValueListEditor.Values['修改日期']:=(Sender as TRTFP).Tag['修改日期'];
 
-  Self.PropertiesValueListEditor.Values['属性组00']:=(Sender as TRTFP).Tag['属性组00'];
-  Self.PropertiesValueListEditor.Values['属性组01']:=(Sender as TRTFP).Tag['属性组01'];
-  Self.PropertiesValueListEditor.Values['属性组02']:=(Sender as TRTFP).Tag['属性组02'];
-
-
-
+  //Self.PropertiesValueListEditor.Values['属性组00']:=(Sender as TRTFP).Tag['属性组00'];
+  //Self.PropertiesValueListEditor.Values['属性组01']:=(Sender as TRTFP).Tag['属性组01'];
+  //Self.PropertiesValueListEditor.Values['属性组02']:=(Sender as TRTFP).Tag['属性组02'];
 
 end;
+
+procedure TFormDesktop.FirstEdit(Sender:TObject);
+begin
+  Self.Caption:=C_SOFTWARE_NAME+' - '+(Sender as TRTFP).Title + ' *';
+  Self.MenuItem_project_save.Enabled:=true;
+end;
+
+procedure TFormDesktop.Clear(Sender:TObject);
+begin
+  Self.Caption:=C_SOFTWARE_NAME;
+  Self.PropertiesValueListEditor.Clear;
+end;
+
+procedure TFormDesktop.ProjectOpenDone(Sender:TObject);
+begin
+  Self.Validate(Sender);
+  Self.MenuItem_project_new.Enabled:=false;
+  Self.MenuItem_project_open.Enabled:=false;
+  Self.MenuItem_project_save.Enabled:=false;
+  Self.MenuItem_project_saveas.Enabled:=true;
+  Self.MenuItem_project_close.Enabled:=true;
+  Self.MenuItem_project_zip.Enabled:=true;
+  Self.MenuItem_project_unzip.Enabled:=false;
+  Self.MenuItem_project_check.Enabled:=true;
+
+end;
+
+procedure TFormDesktop.ProjectCloseDone(Sender:TObject);
+begin
+  Self.Clear(Sender);
+  Self.MenuItem_project_new.Enabled:=true;
+  Self.MenuItem_project_open.Enabled:=true;
+  Self.MenuItem_project_save.Enabled:=false;
+  Self.MenuItem_project_saveas.Enabled:=false;
+  Self.MenuItem_project_close.Enabled:=false;
+  Self.MenuItem_project_zip.Enabled:=false;
+  Self.MenuItem_project_unzip.Enabled:=true;
+  Self.MenuItem_project_check.Enabled:=false;
+
+end;
+
+procedure TFormDesktop.ProjectSaveDone(Sender:TObject);
+begin
+  Self.Validate(Sender);
+  Self.MenuItem_project_save.Enabled:=false;
+end;
+
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -135,16 +205,29 @@ end;
 procedure TFormDesktop.MenuItem_project_openClick(Sender: TObject);
 begin
 
+  if assigned(CurrentRTFP) then
+  begin
+    if CurrentRTFP.IsOpen then CurrentRTFP.Close;
+    CurrentRTFP.Free;
+  end;
+
+  CurrentRTFP:=TRTFP.Create(FormDesktop);
+
+  Self.EventLink(CurrentRTFP);
+
+  if Self.OpenDialog_Project.Execute then
+    CurrentRTFP.Open(Self.OpenDialog_Project.FileName);
 end;
 
 procedure TFormDesktop.MenuItem_project_saveasClick(Sender: TObject);
 begin
-
+  if Self.SaveDialog_Project.Execute then
+    CurrentRTFP.SaveAs(Self.SaveDialog_Project.FileName);
 end;
 
 procedure TFormDesktop.MenuItem_project_saveClick(Sender: TObject);
 begin
-
+  CurrentRTFP.Save;
 end;
 
 
@@ -180,8 +263,8 @@ begin
     begin
       if CurrentRTFP.IsOpen then
         begin
-          CurrentRTFP.Title:=Utf8ToWinCP(Self.PropertiesValueListEditor.Values['标题']);
-          CurrentRTFP.User:=Utf8ToWinCP(Self.PropertiesValueListEditor.Values['用户']);
+          CurrentRTFP.Title:=Utf8ToWinCP(Self.PropertiesValueListEditor.Values['工程标题']);
+          CurrentRTFP.User:=Utf8ToWinCP(Self.PropertiesValueListEditor.Values['创建用户']);
         end;
     end;
 end;
@@ -189,9 +272,20 @@ end;
 procedure TFormDesktop.FormCreate(Sender: TObject);
 begin
   Self.Frame_AufScript1.AufGenerator;
-
+  AufScriptFuncDefineRTFP(Self.Frame_AufScript1.Auf);
+  Self.Frame_AufScript1.HighLighterReNew;
 
   //CurrentRTFP:=TRTFP.Create(Self);
+end;
+
+procedure TFormDesktop.FormDropFiles(Sender: TObject;
+  const FileNames: array of String);
+var len:integer;
+begin
+  len:=Length(FileNames);
+  //Self.Panel_Release.Caption:=IntToStr(len);
+  if len=1 then Self.Panel_Release.Caption:=FileNames[0]
+  else Self.Panel_Release.Caption:='多个文件';
 end;
 
 procedure TFormDesktop.FormClose(Sender: TObject; var CloseAction: TCloseAction
@@ -203,7 +297,8 @@ end;
 procedure TFormDesktop.FormCloseQuery(Sender: TObject; var CanClose: boolean);
 begin
   if assigned(CurrentRTFP) then begin
-    if not CurrentRTFP.Close then CanClose:=false;
+    if CurrentRTFP.IsOpen then
+      if not CurrentRTFP.Close then CanClose:=false;
   end;
 end;
 
