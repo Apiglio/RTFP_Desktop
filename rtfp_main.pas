aa15d949a5b95f16daaa082d2b894328236cc183
Apiglio <553724286@qq.com>
2023-10-08 15:05:25 +0800

    修改与类有关的aufunc指令; 新增主表排序和筛选; 删除早前的排序方法

--- rtfp_main.pas
+++ rtfp_main.pas
@@ -17,7 +17,7 @@ uses
   RTFP_definition, rtfp_constants, rtfp_type, sync_timer, source_dialog, Types;
 
 const
-  C_VERSION_NUMBER  = '0.3.2-alpha.3';
+  C_VERSION_NUMBER  = '0.3.2-alpha.4';
   C_SOFTWARE_NAME   = 'RTFP Desktop';
   C_SOFTWARE_AUTHOR = 'Apiglio';
 
